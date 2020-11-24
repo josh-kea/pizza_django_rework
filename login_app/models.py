08120452793b28956bb27aa47fa179acd6af4475
Joshua Kaplan <joshkap2015@gmail.com>
2020-11-24 15:24:32 +0100

    Adds user login and logout baseline system

---
+++ login_app/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+from django.contrib.auth.models import User
+from secrets import token_urlsafe
+
+
+class PasswordResetRequest(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    secret = models.CharField(max_length=43, default=token_urlsafe)
+    created_timestamp = models.DateTimeField(auto_now_add=True)
+    updated_timestamp = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.user} - {self.created_timestamp}'
+
+
+class UserProfile(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    telephone_number = models.IntegerField(default=12345678)
+    RANKS = (
+        ('Basic', 'Basic'),
+        ('Silver', 'Silver'),
+        ('Gold', 'Gold'),
+    )
+    user_rank = models.CharField(
+        choices=RANKS, default='Basic', max_length=250)
+
+    def __str__(self):
+        return f'{self.user}'
---
+++ login_app/views.py
@@ -0,0 +1,99 @@
+from django.shortcuts import render, reverse
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login as dj_login, logout as dj_logout
+from django.http import HttpResponseRedirect
+from . import models
+from .models import UserProfile
+from django.contrib.auth.decorators import login_required
+
+
+def login(request):
+    context = {}
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user:
+            dj_login(request, user)
+            return HttpResponseRedirect(reverse('account_app:index'))
+        else:
+            context = {'error': 'Bad username or password.'}
+    return render(request, 'login_app/login.html', context)
+
+
+def logout(request):
+    dj_logout(request)
+    return HttpResponseRedirect(reverse('login_app:login'))
+
+
+def password_reset(request):
+    context = {}
+
+    if request.method == "POST":
+        email = request.POST['email']
+        user = User.objects.get(email=email)
+        reset_request = models.PasswordResetRequest()
+        reset_request.user = user
+        reset_request.save()
+        url = reverse('login_app:password_reset_secret',
+                      args=[f'{reset_request.secret}'])
+        url = f'{request.scheme}://{request.META["HTTP_HOST"]}{url}'
+        print(url)
+        context = {
+            'message': 'Please click the link in the email we sent to you.'}
+
+    return render(request, 'login_app/password_reset.html', context)
+
+
+def password_reset_secret(request, secret):
+    context = {'secret': secret}
+    return render(request, 'login_app/password_reset_form.html', context)
+
+
+def password_reset_form(request):
+    email = request.POST['email']
+    password = request.POST['password']
+    confirm_password = request.POST['confirm_password']
+    secret = request.POST['secret']
+
+    user = User.objects.get(email=email)
+    reset_request = models.PasswordResetRequest.objects.get(
+        user=user, secret=secret)
+    if password == confirm_password:
+        user.set_password(password)
+        user.save()
+        reset_request.save()
+        return HttpResponseRedirect(reverse('login_app:login'))
+    context = {
+        'error': 'Something went wrong, try again, don\'t screw up this time!'}
+    return render(request, 'login_app/password_reset_form.html', context)
+
+
+def sign_up(request):
+    context = {}
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        confirm_password = request.POST['confirm_password']
+        email = request.POST['email']
+        if password == confirm_password:
+            if User.objects.create_user(username, email, password):
+                return HttpResponseRedirect(reverse('login_app:login'))
+            else:
+                context = {
+                    'error': 'Could not create user - try somethig else.'}
+        else:
+            context = {'error': 'Passwords do not match.'}
+    return render(request, 'login_app/sign_up.html', context)
+
+
+def user_profile(request):
+    userProfiles = UserProfile.objects.filter(user=request.user)
+    context = {
+        'userProfiles': userProfiles,
+    }
+    return render(request, 'login_app/user_profile.html', context)
+
+
+def delete_account(request):
+    pass
