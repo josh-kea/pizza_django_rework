6a7167ba76b5f0f988b7d978d3971c62f4ec6475
Joshua Kaplan <joshkap2015@gmail.com>
2020-12-11 16:06:02 +0100

    Comments out unnecessary code

--- pizza_app/permissions.py
+++ pizza_app/permissions.py
@@ -1,10 +1,13 @@
 from rest_framework import permissions
-
+from django.contrib.auth.models import User
+from .models import UserProfile
 
 class IsEmployeeOrNoAccess(permissions.BasePermission):
 
    def has_object_permission(self, request, view, obj):
         if request.method in permissions.SAFE_METHODS:
-            return False
+            return True
 
-        return obj.user == request.user
+        # userProfile = User.objects.get(user=request.user)
+        # if userProfile.status == 'employee':
+        #     return obj.user == request.user
