66ced5fc19651966bb9cb63123715e72f43a61ce
Amber Theil <ambe0012@stud.kea.dk>
2020-12-11 11:50:14 +0100

    Added signals to the orders

--- pizza_app/signals.py
+++ pizza_app/signals.py
@@ -1,7 +1,7 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.contrib.auth.models import User
-from . models import UserProfile
+from . models import UserProfile, Order
 
 
 @receiver(post_save, sender=User, dispatch_uid="create_user_profile")
@@ -13,3 +13,14 @@ def create_user_profile(sender, instance, **kwargs):
         user_profile = UserProfile()
         user_profile.user = instance
         user_profile.save()
+
+
+@receiver(post_save, sender=Order, dispatch_uid="create_order")
+def create_order(sender, instance, **kwargs):
+    print("**** order created")
+    print(sender)
+    print(kwargs)
+    if not Order.objects.exists():
+        order = Order()
+        order.order_id = instance
+        order.save()
