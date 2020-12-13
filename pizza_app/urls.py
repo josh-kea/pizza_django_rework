17a7ddbf09a465b0479ae014139178b9f9fe1fd2
Amber Theil <ambe0012@stud.kea.dk>
2020-12-13 20:46:03 +0100

    Added edit page and shows userpage

--- pizza_app/urls.py
+++ pizza_app/urls.py
@@ -5,11 +5,14 @@ from . import views
 app_name = "pizza_app"
 
 urlpatterns = [
-    path('', views.customer_page, name='customer_page'),
+    path('customer_page', views.customer_page, name='customer_page'),
     path('employee_page/', views.employee_page, name='employee_page'),
     path('user_profile/', views.user_profile, name='user_profile'),  # new
     path('base/', views.base, name='base'),
     path('edit_pizza/<int:pizza_id>/', views.edit_pizza, name='edit_pizza'),
+    path('delete_pizza/', views.delete_pizza, name='delete_pizza'),
+    path('update_pizza/', views.update_pizza, name='update_pizza'),
     path('thank_you/', views.thank_you, name='thank_you'),
+    path('edit_customers/', views.edit_customers, name='edit_customers'),
 
 ]
